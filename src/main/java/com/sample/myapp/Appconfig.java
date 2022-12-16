ffe218a6699440b5656bce4469b832730367e5b7
OEUNN <jyejyp5@gmail.com>
2022-12-16 17:53:25 +0900

    DB연결

--- src/main/java/com/sample/myapp/Appconfig.java
+++ src/main/java/com/sample/myapp/Appconfig.java
@@ -1,10 +1,7 @@
 package com.sample.myapp;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ImportResource;
 
 @Configurable
 //@ComponentScan(basePackages= {"com.sample.myapp"}) //어노테이션
